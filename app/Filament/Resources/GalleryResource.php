4722e087178eaec056cb1b20931107e7ea7dd49c
thriflab <ggiffi03@gmail.com>
2025-01-22 13:52:52 +0700

    Gallery

--- app/Filament/Resources/GalleryResource.php
+++ app/Filament/Resources/GalleryResource.php
@@ -13,6 +13,8 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -49,7 +51,10 @@ class GalleryResource extends Resource
     {
         return $table
             ->columns([
-                //
+                ImageColumn::make('foto')
+                ->circular(),
+                TextColumn::make('nama_acara'),
+                TextColumn::make('tanggal')
             ])
             ->filters([
                 //
